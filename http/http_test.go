ab307832bfc9650cd504bb81534f415c9832ecec
sters <sters@users.noreply.github.com>
2021-01-17 19:38:53 +0900

    fix

--- http/http_test.go
+++ http/http_test.go
@@ -2,28 +2,7 @@ package http
 
 import (
 	"testing"
-	"time"
 )
-
-func TestRun(t *testing.T) {
-	s, err := NewServer("9999")
-	if err != nil {
-		t.Fatalf("failed to NewServer: %+v", err)
-	}
-
-	go func() {
-		err := s.Run()
-		if err != nil {
-			t.Errorf("failed to Run: %+v", err)
-		}
-	}()
-
-	time.Sleep(200 * time.Millisecond)
-
-	if err := s.Close(); err != nil {
-		t.Fatalf("failed to Close: %+v", err)
-	}
-}
 
 func TestRunFail(t *testing.T) {
 	s, err := NewServer("aaa")
@@ -31,14 +10,9 @@ func TestRunFail(t *testing.T) {
 		t.Fatalf("failed to NewServer: %+v", err)
 	}
 
-	go func() {
-		err := s.Run()
-		if err == nil {
-			t.Errorf("Run want to fail")
-		}
-	}()
-
-	time.Sleep(100 * time.Millisecond)
+	if err := s.Run(); err == nil {
+		t.Errorf("Run want to fail")
+	}
 
 	if err := s.Close(); err != nil {
 		t.Fatalf("failed to Close: %+v", err)
